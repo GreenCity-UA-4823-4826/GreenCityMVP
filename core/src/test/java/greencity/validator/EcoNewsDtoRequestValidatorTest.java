b6f93bc4cef4b0a82ed9b22715bacdce3cf28941
Oleh-323 <oleh.vlas.323@gmail.com>
2026-06-10 03:54:19 +0300

    add tests for empty and null to EcoNewsDtoRequestValidatorTest

--- core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
+++ core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
@@ -1,10 +1,13 @@
 package greencity.validator;
 
 import greencity.dto.econews.AddEcoNewsDtoRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static greencity.ModelUtils.getAddEcoNewsDtoRequest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,10 +17,33 @@ class EcoNewsDtoRequestValidatorTest {
     @InjectMocks
     private EcoNewsDtoRequestValidator validator;
 
+    @BeforeEach
+    void setup() {
+        validator= new EcoNewsDtoRequestValidator();
+    }
+
     @Test
     void isValidTrueTest() {
         AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
         request.setSource("https://eco-lavca.ua/");
         assertTrue(validator.isValid(request, null));
     }
+
+    @Test
+    void isValidTrueWhenSourceIsNullTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setSource(null);
+        request.setTags(List.of("News"));
+        assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValidTrueWhenSourceIsEmptyTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setSource("");
+        request.setTags(List.of("News"));
+        assertTrue(validator.isValid(request, null));
+    }
+
+
 }
