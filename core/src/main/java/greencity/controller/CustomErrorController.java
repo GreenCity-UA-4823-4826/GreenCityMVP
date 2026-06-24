1da9bb9f5544b44ca4aa740e30ec97d5e057a587
IrynaVdovych <vdovych.iryna@gmail.com>
2026-06-24 23:11:44 +0300

    [49] fix GET error response code

---
+++ core/src/main/java/greencity/controller/CustomErrorController.java
@@ -0,0 +1,39 @@
+package greencity.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("${server.error.path:${error.path:/error}}")
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (statusCode == null) {
+            return ResponseEntity.ok(Map.of(
+                    "status", HttpStatus.OK.value(),
+                    "error", HttpStatus.OK.getReasonPhrase()
+            ));
+        }
+
+        HttpStatus status = HttpStatus.resolve(Integer.parseInt(statusCode.toString()));
+
+        if (status == null) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return ResponseEntity.status(status).body(Map.of(
+                "status", status.value(),
+                "error", status.getReasonPhrase()
+        ));
+    }
+}
