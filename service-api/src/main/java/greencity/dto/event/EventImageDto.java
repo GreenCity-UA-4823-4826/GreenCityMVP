79e64c5fbc462efbb4fd855ea2106e8d6424dca8
IrynaVdovych <vdovych.iryna@gmail.com>
2026-06-28 18:27:02 +0300

    [90] add DTO classes for create event request and response

---
+++ service-api/src/main/java/greencity/dto/event/EventImageDto.java
@@ -0,0 +1,13 @@
+package greencity.dto.event;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EventImageDto {
+
+    private String imageUrl;
+
+    private boolean mainImage;
+}
