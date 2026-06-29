5579a493d879345384a359b7c338123c267da126
IrynaVdovych <vdovych.iryna@gmail.com>
2026-06-29 12:13:20 +0300

    [90] add Event mappers for request and response DTO conversion

--- service-api/src/main/java/greencity/dto/event/EventResponseDto.java
+++ service-api/src/main/java/greencity/dto/event/EventResponseDto.java
@@ -4,6 +4,7 @@ import greencity.enums.event.EventType;
 import greencity.enums.event.EventVisibility;
 import greencity.enums.event.InitiativeType;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 
 @Getter
 @Setter
+@Builder
 public class EventResponseDto {
 
     private Long id;
