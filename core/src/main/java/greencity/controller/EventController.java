2a504d9e577f72eb1b1762617edfb1ee153d9848
IrynaVdovych <vdovych.iryna@gmail.com>
2026-07-02 13:48:21 +0300

    [94] fix imports and response type in EventController

--- core/src/main/java/greencity/controller/EventController.java
+++ core/src/main/java/greencity/controller/EventController.java
@@ -21,7 +21,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -79,8 +86,8 @@ public class EventController {
             @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
     })
     @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping (value = "/{eventId}")
-    public ResponseEntity<Object> deleteEvent(@PathVariable Long eventId,
+    @DeleteMapping(value = "/{eventId}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable Long eventId,
                                               @Parameter(hidden = true) @CurrentUser UserVO userVO) {
         eventService.deleteEvent(eventId, userVO);
         return ResponseEntity.ok().build();
