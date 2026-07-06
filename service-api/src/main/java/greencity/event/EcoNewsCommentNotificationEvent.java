e94a83b6b9383792d4f842328f38a62ece6d7ef2
Orest <orestovsky@gmail.com>
2026-07-06 18:56:16 +0300

    Added notification API contracts and enums

---
+++ service-api/src/main/java/greencity/event/EcoNewsCommentNotificationEvent.java
@@ -0,0 +1,16 @@
+package greencity.event;
+
+import greencity.dto.user.UserVO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class EcoNewsCommentNotificationEvent {
+    private final UserVO author;
+    private final UserVO commenter;
+    private final Long ecoNewsId;
+    private final String newsTitle;
+}
---
+++ service-api/src/main/java/greencity/service/UserNotificationService.java
@@ -0,0 +1,26 @@
+package greencity.service;
+
+import greencity.dto.PageableAdvancedDto;
+import greencity.dto.notification.ActionDto;
+import greencity.dto.notification.NotificationDto;
+import greencity.dto.user.UserVO;
+import greencity.enums.NotificationType;
+import greencity.enums.ProjectName;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+public interface UserNotificationService {
+    PageableAdvancedDto<NotificationDto> getNotificationsFiltered(Long userId, Pageable pageable, String language,
+        ProjectName projectName, List<NotificationType> notificationTypes, Boolean viewed);
+
+    void notificationSocket(ActionDto user);
+
+    void createNotification(UserVO targetUser, UserVO actionUser, NotificationType notificationType,
+        Long targetId, String secondMessageText);
+
+    void deleteNotification(Long userId, Long notificationId);
+
+    void unreadNotification(Long notificationId);
+
+    void viewNotification(Long notificationId);
+}
