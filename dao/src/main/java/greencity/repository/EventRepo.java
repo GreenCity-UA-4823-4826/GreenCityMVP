37335c3255c64ec948c8a10904558bb955c7fdda
IrynaVdovych <vdovych.iryna@gmail.com>
2026-07-13 16:35:35 +0300

    [116] Add searchByTitle and findTitleSuggestions query methods to EventRepo

--- dao/src/main/java/greencity/repository/EventRepo.java
+++ dao/src/main/java/greencity/repository/EventRepo.java
@@ -2,6 +2,8 @@ package greencity.repository;
 
 import greencity.entity.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,20 @@ import java.util.List;
 @Repository
 public interface EventRepo extends JpaRepository<Event, Long> {
     List<Event> findByOrganizerId(Long organizerId);
+
+    @Query("SELECT e FROM Event e " +
+            "WHERE LOWER(e.title) LIKE LOWER(CONCAT('%', :query, '%')) " +
+            "ORDER BY CASE " +
+            "WHEN LOWER(e.title) = LOWER(:query) THEN 1 " +
+            "WHEN LOWER(e.title) LIKE LOWER(CONCAT(:query, '%')) THEN 2 " +
+            "ELSE 3 END")
+    List<Event> searchByTitle(@Param("query") String query);
+
+    @Query("SELECT e.id, e.title FROM Event e " +
+            "WHERE LOWER(e.title) LIKE LOWER(CONCAT('%', :query, '%')) " +
+            "ORDER BY CASE " +
+            "WHEN LOWER(e.title) = LOWER(:query) THEN 1 " +
+            "WHEN LOWER(e.title) LIKE LOWER(CONCAT(:query, '%')) THEN 2 " +
+            "ELSE 3 END")
+    List<Object[]> findTitleSuggestions(@Param("query") String query);
 }
