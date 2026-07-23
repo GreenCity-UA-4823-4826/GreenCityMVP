7d8e594e9ed2558db5be312ca540c772b8931d73
IrynaVdovych <vdovych.iryna@gmail.com>
2026-07-23 18:15:23 +0300

    [88] Add searchFriends, addFriend, cancelFriendRequest to FriendshipService

--- service-api/src/main/java/greencity/service/FriendshipService.java
+++ service-api/src/main/java/greencity/service/FriendshipService.java
@@ -1,6 +1,9 @@
 package greencity.service;
 
+import greencity.dto.user.UserFriendDto;
 import greencity.dto.user.UserVO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface FriendshipService {
     /**
@@ -10,4 +13,30 @@ public interface FriendshipService {
      * @return number of friends as {@code long}.
      */
     long countOfUserFriends(UserVO userVO);
+
+    /**
+     * Method for searching users by name.
+     *
+     * @param userVO   - current authorized user.
+     * @param query    - search string to filter by name.
+     * @param pageable - pagination parameters.
+     * @return {@link Page} of {@link UserFriendDto}.
+     */
+    Page<UserFriendDto> searchFriends(UserVO userVO, String query, Pageable pageable);
+
+    /**
+     * Method for sending a friend request to another user.
+     *
+     * @param userVO     - current authorized user.
+     * @param receiverId - id of the user to send request to.
+     */
+    void addFriend(UserVO userVO, Long receiverId);
+
+    /**
+     * Method for cancelling a friend request sent by current user.
+     *
+     * @param userVO     - current authorized user.
+     * @param receiverId - id of the user to cancel request for.
+     */
+    void cancelFriendRequest(UserVO userVO, Long receiverId);
 }
