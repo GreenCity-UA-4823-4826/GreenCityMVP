c27d8be418b11a73d35814fa7ebf981bce0c83a8
IrynaVdovych <vdovych.iryna@gmail.com>
2026-07-23 18:23:11 +0300

    [88] Add UserFriendDtoMapper

---
+++ service/src/main/java/greencity/mapping/UserFriendDtoMapper.java
@@ -0,0 +1,21 @@
+package greencity.mapping;
+
+import greencity.dto.user.UserFriendDto;
+import greencity.enums.friendship.FriendshipStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFriendDtoMapper {
+    public UserFriendDto toDto(Object[] row) {
+        return UserFriendDto.builder()
+                .id(((Number) row[0]).longValue())
+                .name((String) row[1])
+                .profilePicturePath((String) row[2])
+                .rating(row[3] != null ? ((Number) row[3]).doubleValue() : null)
+                .city((String) row[4])
+                .mutualFriendsCount(row[5] != null ? ((Number) row[5]).longValue() : 0L)
+                .friendshipStatus(row[6] != null ? FriendshipStatus.valueOf((String) row[6]) : null)
+                .requestedByCurrentUser(Boolean.TRUE.equals(row[7]))
+                .build();
+    }
+}
