b4eb900a8ac85d73d565c833c1bb1e9476b9bfdf
artem <artemisenko73@gmail.com>
2026-07-26 13:48:01 +0300

    feat(habit-comment): add DTOs and mapper for habit comments

---
+++ service-api/src/main/java/greencity/dto/habitcomment/HabitCommentDto.java
@@ -0,0 +1,31 @@
+package greencity.dto.habitcomment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+public class HabitCommentDto {
+    @NotNull
+    @Min(1)
+    private Long id;
+
+    private LocalDateTime createdDate;
+
+    private HabitCommentAuthorDto author;
+
+    private String text;
+}
